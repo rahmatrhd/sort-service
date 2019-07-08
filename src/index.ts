e5b7b25a73ced8bf47594f29b4b4f2a7ab0284d4
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2019-07-09 00:23:59 +0700

    add body-parser and morgan middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
+import * as bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
 import * as express from 'express';
+import * as morgan from 'morgan';
+
+dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
 app.listen(process.env.PORT || 3000, () => {
   // tslint:disable-next-line:no-console
